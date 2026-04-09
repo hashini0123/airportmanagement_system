f0e40c4c1eb6a97ac0c3e5a2eca862098c0139fd
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-09 23:48:25 +0530

    create # 6

--- src/main/java/edu/icet/controller/AirLineController.java
+++ src/main/java/edu/icet/controller/AirLineController.java
@@ -1,4 +1,29 @@
 package edu.icet.controller;
 
+import edu.icet.model.dto.AirLineDTO;
+import edu.icet.service.AirLineService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
 public class AirLineController {
+
+    private final AirLineService airLineService;
+
+    @PostMapping("/add")
+    public boolean addAirLine(@RequestBody AirLineDTO airLineDTO){
+        return airLineService.addAirLine(airLineDTO);
+    }
+
+    @PutMapping("/update")
+    public boolean updateAirLine(@RequestBody AirLineDTO airLineDTO){
+        return airLineService.updateAirLine(airLineDTO);
+    }
+
+    @DeleteMapping("/delete-by-id/{id}")
+    public boolean deleteAirLine(@PathVariable String id){
+        return airLineService.deleteById(id);
+    }
 }
