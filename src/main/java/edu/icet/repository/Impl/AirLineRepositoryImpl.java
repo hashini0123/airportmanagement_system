26a899431fb967590f0bf308e427c6fb1b1da7c2
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-11 00:16:02 +0530

    create # 20

--- src/main/java/edu/icet/repository/Impl/AirLineRepositoryImpl.java
+++ src/main/java/edu/icet/repository/Impl/AirLineRepositoryImpl.java
@@ -1,4 +1,28 @@
 package edu.icet.repository.Impl;
 
-public class AirLineRepositoryImpl {
+import edu.icet.model.dto.AirLineDTO;
+import edu.icet.repository.AirLineRepository;
+
+import java.util.List;
+
+public class AirLineRepositoryImpl implements AirLineRepository {
+    @Override
+    public boolean addAirLine(AirLineDTO airLineDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean updateAirLine(AirLineDTO airLineDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        return false;
+    }
+
+    @Override
+    public List<AirLineDTO> getAll() {
+        return List.of();
+    }
 }
