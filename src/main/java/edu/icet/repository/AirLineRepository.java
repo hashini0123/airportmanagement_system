5ae1883b8a67da9012fd33264250d2b506f52851
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-10 23:23:01 +0530

    create # 19

--- src/main/java/edu/icet/repository/AirLineRepository.java
+++ src/main/java/edu/icet/repository/AirLineRepository.java
@@ -4,8 +4,16 @@ import edu.icet.model.dto.AirLineDTO;
 import edu.icet.model.entity.AirLineEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AirLineRepository {
 
 
     boolean addAirLine(AirLineDTO airLineDTO);
+
+    boolean updateAirLine(AirLineDTO airLineDTO);
+
+    boolean deleteById(String id);
+
+    List<AirLineDTO> getAll();
 }
