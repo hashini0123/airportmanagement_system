bde893f3a5be180e3c8b94fc1dcd7347360612ed
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-23 14:42:31 +0530

    create # 40

--- src/main/java/edu/icet/service/Impl/FlightServiceImpl.java
+++ src/main/java/edu/icet/service/Impl/FlightServiceImpl.java
@@ -1,14 +1,22 @@
 package edu.icet.service.Impl;
 
 import edu.icet.model.dto.FlightDTO;
+import edu.icet.repository.FlightRepository;
 import edu.icet.service.FlightService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@AllArgsConstructor
 public class FlightServiceImpl implements FlightService {
+
+    private final FlightRepository flightRepository;
+
     @Override
     public boolean addFlight(FlightDTO flightDTO) {
-        return false;
+        return flightRepository.addFlight(flightDTO);
     }
 
     @Override
