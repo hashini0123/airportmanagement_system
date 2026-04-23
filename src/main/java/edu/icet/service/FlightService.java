3eb7e2661741e4637af2bd534107b3c2a233539c
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-23 14:37:55 +0530

    create # 37

--- src/main/java/edu/icet/service/FlightService.java
+++ src/main/java/edu/icet/service/FlightService.java
@@ -2,10 +2,14 @@ package edu.icet.service;
 
 import edu.icet.model.dto.FlightDTO;
 
+import java.util.List;
+
 public interface FlightService {
     boolean addFlight(FlightDTO flightDTO);
 
     boolean updateFligh(FlightDTO flightDTO);
 
     boolean deleteById(String id);
+
+    List<FlightDTO> getAll();
 }
