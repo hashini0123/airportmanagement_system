f924184afde57227659a67c53684cae372fd223a
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-23 14:39:21 +0530

    create # 38

--- src/main/java/edu/icet/service/Impl/FlightServiceImpl.java
+++ src/main/java/edu/icet/service/Impl/FlightServiceImpl.java
@@ -1,4 +1,28 @@
 package edu.icet.service.Impl;
 
-public class FlightServiceImpl {
+import edu.icet.model.dto.FlightDTO;
+import edu.icet.service.FlightService;
+
+import java.util.List;
+
+public class FlightServiceImpl implements FlightService {
+    @Override
+    public boolean addFlight(FlightDTO flightDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean updateFligh(FlightDTO flightDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        return false;
+    }
+
+    @Override
+    public List<FlightDTO> getAll() {
+        return List.of();
+    }
 }
