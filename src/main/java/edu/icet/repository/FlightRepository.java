31332f2693a5ea1e4a034712f224a3d5689922a5
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-24 21:19:52 +0530

    create # 42

--- src/main/java/edu/icet/repository/FlightRepository.java
+++ src/main/java/edu/icet/repository/FlightRepository.java
@@ -2,6 +2,12 @@ package edu.icet.repository;
 
 import edu.icet.model.dto.FlightDTO;
 
+import java.util.List;
+
 public interface FlightRepository {
+
     boolean addFlight(FlightDTO flightDTO);
+
+
+    boolean updateFlight(FlightDTO flightDTO);
 }
