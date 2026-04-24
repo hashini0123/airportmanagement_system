03876bceee7703f06fe7d528d49614db86e880b6
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-24 21:20:04 +0530

    create # 43

--- src/main/java/edu/icet/repository/Impl/FlightRepositoryImpl.java
+++ src/main/java/edu/icet/repository/Impl/FlightRepositoryImpl.java
@@ -1,4 +1,10 @@
 package edu.icet.repository.Impl;
 
+import edu.icet.model.dto.FlightDTO;
+import edu.icet.repository.FlightRepository;
+
+import java.util.List;
+
 public class FlightRepositoryImpl {
+
 }
